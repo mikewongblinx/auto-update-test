f5adb9f4813511eb4588724d01159e78d0776a9c
Mike Wong <mike.wong@blinxsolutions.com>
2024-01-21 12:41:38 +0000

    FC

---
+++ forge.config.js
@@ -0,0 +1,42 @@
+module.exports = {
+  packagerConfig: {
+    asar: true,
+  },
+  publishers: [
+    {
+      name: '@electron-forge/publisher-github',
+      config: {
+        repository: {
+          owner: 'mikewongblinx',
+          name: 'auto-update-test'
+        },
+        prerelease: true
+      }
+    }
+  ],
+  rebuildConfig: {},
+  makers: [
+    {
+      name: '@electron-forge/maker-squirrel',
+      config: {},
+    },
+    {
+      name: '@electron-forge/maker-zip',
+      platforms: ['darwin'],
+    },
+    {
+      name: '@electron-forge/maker-deb',
+      config: {},
+    },
+    {
+      name: '@electron-forge/maker-rpm',
+      config: {},
+    },
+  ],
+  plugins: [
+    {
+      name: '@electron-forge/plugin-auto-unpack-natives',
+      config: {},
+    },
+  ],
+};
